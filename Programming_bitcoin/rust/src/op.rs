f2ace81f5287f77fe91e9d747b56c89bac2fff0e
Shourya742 <sshourya17@gmail.com>
2023-10-30 21:51:13 +0530

    Added decode num in op

--- Programming_bitcoin/rust/src/op.rs
+++ Programming_bitcoin/rust/src/op.rs
@@ -40,3 +40,31 @@ pub fn parse_op_codes(op_code:u32)->OpCodeFunctions {
         _=>panic!("Unknown opCode"),
     }
 }
+
+
+fn decode_num(element:Vec<u8>)->i32 {
+    if element == b"".to_vec() {
+        return 0;
+    }
+    let mut result:i32;
+    let mut big_endian = element.clone();
+    big_endian.reverse();
+    let negative:bool;
+    if(big_endian[0] & 0x80) == 1 {
+        negative = true;
+        result = BigEndian::read_u32(&[0,0,0,big_endian[0] & 0x7f]) as i32;
+    } else {
+        negative = false;
+        result = BigEndian::read_i32(&[0,0,0,big_endian[0]]) as i32;
+    }
+    for c in &big_endian[1..] {
+        result <<=8;
+        result+=BigEndian::read_i32(&[0,0,0,c.clone()]) as i32;
+    }
+
+    if negative {
+        return -result;
+    } else {
+        return result;
+    }
+}
